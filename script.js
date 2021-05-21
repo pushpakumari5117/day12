261de8f22a07a63668aa5eec380d37f4ede4208f
Pushpa Kumari <68471073+pushpakumari5117@users.noreply.github.com>
2021-05-21 12:40:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-const main = document.getElementById('main')
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=cfa599ffffdb7fd43099b455909639e8&page=1'
-const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=cfa599ffffdb7fd43099b455909639e8&query="'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const  form = document.getElementById('form')
 const search=document.getElementById('search')
+const main = document.getElementById('main')
 getMovies(API_URL)
 async function getMovies(url) {
   const res = await fetch(url)
   const data = await res.json()
   showMovies(data.results)
 }
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const searchTerm = search.value
+  if(searchTerm && searchTerm !== '') {
+    getMovies(SEARCH_API + searchTerm)
+    search.value = ''
+  }
+  else{
+    window.location.reload()
+  }
+})
+
 function showMovies(movies){
   main.innerHTML = ''
   movies.forEach((movie) => {
@@ -41,14 +53,3 @@ function getClassByRate(vote) {
     return 'red'
   }
 }
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  const searchTerm = search.value
-  if(searchTerm && searchTerm !== '') {
-    getMovies(SEARCH_API + searchTerm)
-    search.value = ''
-  }
-  else{
-    window.location.reload()
-  }
-})
